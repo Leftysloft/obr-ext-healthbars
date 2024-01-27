c91ace219476137fd2badad119420536319bbf96
Leftysloft <patrickbglose@gmail.com>
2024-01-27 16:09:30 -0500

    Let's make some changes

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ import "./style.css";
 import OBR from "@owlbear-rodeo/sdk";
 import { setupContextMenu } from "./contextMenu";
 import { setupSheetList, sheetFunction } from "./sheetList";
-import { isImage } from "@owlbear-rodeo/sdk";
-
-//
-// const characters = await OBR.scene.items.getItems(
-//   (item) => item.layer === "CHARACTER" && isImage(item)
-// );
-//
 
 document.querySelector("#app").innerHTML = `
 

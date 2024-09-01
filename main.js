ba14ea8baeeccc4f99c5d440d431cd6ac6546109
Leftysloft <patrickbglose@gmail.com>
2024-08-31 23:07:21 -0400

    First round of settings page
    Created my first setting page and moved usage guide to it.

--- main.js
+++ main.js
@@ -2,26 +2,32 @@ import "./style.css";
 import OBR from "@owlbear-rodeo/sdk";
 import { setupContextMenu } from "./contextMenu";
 import { setupSheetList } from "./sheetList";
+import { ID } from "./constants";
 
 document.querySelector("#app").innerHTML = `
-
   <div>
     <ul id="sheet-list"></ul>
   </div>
 `;
 
 OBR.onReady(() => {
+  //build the contect menu
   setupContextMenu();
+
+  //Open popover menu
+  const popoverBtn = document.getElementById("popoverButton");
+  popoverBtn.onclick = () => {
+    console.log("popoverBtn");
+    OBR.popover.open({
+      id: ID,
+      url: "/settings/settings.html",
+      height: 400,
+      width: 400,
+    });
+    console.log("settings");
+  };
+
+  //Set up the view
   setupSheetList(document.querySelector("#sheet-list"));
   OBR.action.open();
 });
-
-//Open Usage Guide window.
-const usageGuide = document.getElementById("usageButton");
-usageGuide.onclick = () => {
-  onclick = window.open(
-    "https://github.com/Leftysloft/obr-ext-healthbars/tree/main#readme",
-    "mozillaWindow",
-    "left=100,top=100,width=600,height=800"
-  );
-};

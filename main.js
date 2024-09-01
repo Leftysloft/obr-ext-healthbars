6e65b95806e86092631675dc8a261e6982b1fac2
Leftysloft <patrickbglose@gmail.com>
2024-08-31 21:19:53 -0400

    Scripting changes
    Moving scripts from HTML to respective js pages.

--- main.js
+++ main.js
@@ -15,3 +15,13 @@ OBR.onReady(() => {
   setupSheetList(document.querySelector("#sheet-list"));
   OBR.action.open();
 });
+
+//Open Usage Guide window.
+const usageGuide = document.getElementById("usageButton");
+usageGuide.onclick = () => {
+  onclick = window.open(
+    "https://github.com/Leftysloft/obr-ext-healthbars/tree/main#readme",
+    "mozillaWindow",
+    "left=100,top=100,width=600,height=800"
+  );
+};

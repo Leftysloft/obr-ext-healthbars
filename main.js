a9fba0ee289a3a2fe5f9ed5d92af17f065db7ac4
Leftysloft <patrickbglose@gmail.com>
2024-09-01 00:27:38 -0400

    stopping for the night

--- main.js
+++ main.js
@@ -10,16 +10,6 @@ document.querySelector("#app").innerHTML = `
   </div>
 `;
 
-//Open Usage Guide window.
-const usageGuide = document.getElementById("usageButton");
-usageGuide.onclick = () => {
-  onclick = window.open(
-    "https://github.com/Leftysloft/obr-ext-healthbars/tree/main#readme",
-    "mozillaWindow",
-    "left=100,top=100,width=600,height=800"
-  );
-};
-
 OBR.onReady(() => {
   //build the contect menu
   setupContextMenu();

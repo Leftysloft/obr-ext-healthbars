01dd971306ca07c13f50ab0a425aae4cbe863678
Leftysloft <patrickbglose@gmail.com>
2024-08-31 23:53:13 -0400

    try again

--- main.js
+++ main.js
@@ -2,16 +2,32 @@ import "./style.css";
 import OBR from "@owlbear-rodeo/sdk";
 import { setupContextMenu } from "./contextMenu";
 import { setupSheetList } from "./sheetList";
+import { ID } from "./constants";
 
 document.querySelector("#app").innerHTML = `
-
   <div>
     <ul id="sheet-list"></ul>
   </div>
 `;
 
 OBR.onReady(() => {
+  //build the contect menu
   setupContextMenu();
+
+  //Open popover menu
+  const popoverBtn = document.getElementById("popoverButton");
+  popoverBtn.onclick = () => {
+    console.log("popoverBtn");
+    OBR.popover.open({
+      id: ID,
+      url: "/settings/settings.html",
+      height: 400,
+      width: 400,
+    });
+    console.log("settings");
+  };
+
+  //Set up the view
   setupSheetList(document.querySelector("#sheet-list"));
   OBR.action.open();
 });

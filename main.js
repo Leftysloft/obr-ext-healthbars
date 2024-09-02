c187cf4e691f5685f4dfa77230d6c7e515bdb635
Leftysloft <patrickbglose@gmail.com>
2024-09-02 12:50:11 -0400

    Revert "Update main.js"
    This reverts commit 10d52f9dfab19d812d5e6671fc655cd3ae11c196.

--- main.js
+++ main.js
@@ -3,25 +3,21 @@ import OBR from "@owlbear-rodeo/sdk";
 import { setupContextMenu } from "./contextMenu";
 import { setupSheetList } from "./sheetList";
 import { ID } from "./constants";
-// import { settings } from "./settings/settings";
 
 document.querySelector("#app").innerHTML = `
-
   <div>
     <ul id="sheet-list"></ul>
   </div>
 `;
 
 OBR.onReady(() => {
+  //build the contect menu
   setupContextMenu();
-
-  console.log(OBR.scene.onReadyChange(), "onChange");
 
   //Open popover menu
   const popoverBtn = document.getElementById("popoverButton");
   popoverBtn.onclick = () => {
     console.log("popoverBtn");
-    // settings();
     OBR.popover.open({
       id: ID,
       url: "/settings/settings.html",
@@ -31,16 +27,7 @@ OBR.onReady(() => {
     console.log("settings");
   };
 
+  //Set up the view
   setupSheetList(document.querySelector("#sheet-list"));
   OBR.action.open();
 });
-
-//Open Usage Guide window.
-const usageGuide = document.getElementById("usageButton");
-usageGuide.onclick = () => {
-  onclick = window.open(
-    "https://github.com/Leftysloft/obr-ext-healthbars/tree/main#readme",
-    "mozillaWindow",
-    "left=100,top=100,width=600,height=800"
-  );
-};

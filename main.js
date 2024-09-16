7981a33b5fbddf79a0822711b2b6ca87d8e87417
Leftysloft <patrickbglose@gmail.com>
2024-09-16 19:04:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,17 @@ OBR.onReady(() => {
     <ul id="sheet-list"></ul>
   </div>
 `;
+
+  //Open Usage Guide window.
+  const usageGuide = document.getElementById("usageButton");
+  usageGuide.onclick = () => {
+    onclick = window.open(
+      "https://github.com/Leftysloft/obr-ext-healthbars/tree/main#readme",
+      "mozillaWindow",
+      "left=100,top=100,width=600,height=800"
+    );
+  };
+
   setupContextMenu();
 
   setupSheetList(document.querySelector("#sheet-list"));
